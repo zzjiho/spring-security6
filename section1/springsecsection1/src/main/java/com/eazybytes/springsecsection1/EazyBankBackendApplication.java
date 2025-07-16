48089ef81f62584f509e7095da86d62e9af752dd
zzjiho <youngyoungnim@gmail.com>
2025-07-16 23:08:43 +0900

    section1

---
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/EazyBankBackendApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+// @ComponentScan("com.eazybytes.springsecsection1.controller")
+public class EazyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+}
